5ba961bd2620985cc6c58aae0f7baa127041feb0
stackgen-automation <automation@stackgen.com>
2024-09-13 01:34:13 +0000

    Automated commit

---
+++ path/terraform/outputs.tf
@@ -0,0 +1,23 @@
+
+output "azurerm_user_assigned_managed_identity_restpie3_role_this" {
+  value = module.appcd_2d68328d-e774-4ea4-b154-a657585c74d4.this
+  sensitive = false
+}
+
+
+output "azurerm_user_assigned_managed_identity_restpie3_role_principal_id" {
+  value = module.appcd_2d68328d-e774-4ea4-b154-a657585c74d4.principal_id
+  sensitive = false
+}
+
+
+output "azurerm_user_assigned_managed_identity_restpie3_role_id" {
+  value = module.appcd_2d68328d-e774-4ea4-b154-a657585c74d4.id
+  sensitive = false
+}
+
+
+output "azurerm_user_assigned_managed_identity_restpie3_role_client_id" {
+  value = module.appcd_2d68328d-e774-4ea4-b154-a657585c74d4.client_id
+  sensitive = false
+}
